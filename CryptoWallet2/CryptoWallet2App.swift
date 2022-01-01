14915c42833a823affd1503b5f636ad2178dcd00
Eldar Tengizov <75984018+madpuncher@users.noreply.github.com>
2022-01-02 00:29:20 +0300

    Initial Commit

---
+++ CryptoWallet2/CryptoWallet2App.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWallet2App.swift
+//  CryptoWallet2
+//
+//  Created by Eʟᴅᴀʀ Tᴇɴɢɪᴢᴏᴠ on 02.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWallet2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
