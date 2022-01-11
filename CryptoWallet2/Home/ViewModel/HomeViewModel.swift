821ac48827ba8f12ee72882634db2e2316b7c50e
Eldar Tengizov <75984018+madpuncher@users.noreply.github.com>
2022-01-11 14:31:24 +0300

    Filtering data based on search bar text using Combine

--- CryptoWallet2/Home/ViewModel/HomeViewModel.swift
+++ CryptoWallet2/Home/ViewModel/HomeViewModel.swift
@@ -16,10 +16,29 @@ class HomeViewModel: ObservableObject {
     }
     
     private func addSubscribers() {
-        dataService.$coins
-            .sink { coins in
-                self.allCoins = coins
+        $searchText
+            .combineLatest(dataService.$coins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main, options: .none)
+            .map(filterCoins)
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [Coin]) -> [Coin] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        let filteredCoins = coins.filter { coin -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
+        
+        return filteredCoins
     }
 }
